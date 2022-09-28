83383bb093e5877ba03f4f2ae8011116a22ccfc0
Harry Sharma <harshit.sharma@kcl.ac.uk>
2022-09-28 20:59:46 +0100

    Task complete

--- median.py
+++ median.py
@@ -1,5 +1,28 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+"""Median calculator.
+https://www.w3schools.com/python/python_lists.asp was used for help with list syntax
+"""
+def median(number):
+     n=0
+     number.sort()
+     n=len(number)
+ 
+     if(n%2==0):
+         x=n//2
+         mid1=number[x]
+         mid2=number[x-1]
+         median=(mid1+mid2)/2
+         print(f'The median is {median}')
+     else:
+        middle=n//2
+        print (f'The median is {number[middle]}')
+        
+         
+  
+    
+
+    
+    
+    
 
 while True:
     try:
@@ -10,3 +33,7 @@ while True:
     else:
         break
 print(numbers)
+median(numbers)
+
+
+
